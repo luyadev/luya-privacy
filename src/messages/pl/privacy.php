cbd6cd340db93ed0a68f657f3d22452fc649e117
vave <fiolekcs@o2.pl>
2018-08-25 00:19:01 +0200

    Create privacy.php

---
+++ src/messages/pl/privacy.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'privacy_widget.privacy_content' => 'Używamy plików cookie, aby poprawić jakość korzystania z naszej witryny. Przeczytaj i zaakceptuj naszą politykę prywatności.',
+    'privacy_widget.accept_privacy_button_text' => 'Akceptuję',
+    'privacy_widget.decline_privacy_button_text' => 'Odrzucam',
+    
+    'block_privacy.block_name' => 'Prywatność',
+    'block_privacy.privacy_content' => 'Wiadomość o prywatności',
+    'block_privacy.accept_privacy_button_text' => 'Zaakceptuj tekst przycisku',
+    
+];
